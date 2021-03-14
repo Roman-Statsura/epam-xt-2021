3c4a941e0bf364fef348d254ccbb3b02425a9968
Roman Statsura <srs1012@yandex.ru>
2021-03-14 21:12:22 +0400

    added the ability to test my string class

---
+++ Task 2/StringClassLibrary/TestString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StringClassLibrary;
+
+namespace TestString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Str s = new Str("string".ToCharArray());
+            Str s2 = new Str("hi".ToCharArray());
+            s.Print();
+            Str s1 = new Str("privet".ToCharArray());
+            Console.WriteLine(s == s1);
+            Console.WriteLine(s.CastIndexOf('t'));
+            Console.WriteLine(s[0]);
+            Str sum = s + s2;
+            sum.Print();
+            Str st = new Str("string");
+            Str sum1 = st + s;
+            sum1.Print();
+            Str mult = st.StrMultiply(3);
+            mult.Print();
+            Console.WriteLine(st.Equals(s));
+            Console.WriteLine(st.GetHashCode());
+            Console.WriteLine(s.GetHashCode());
+            Console.WriteLine(s == st);
+        }
+    }
+}
