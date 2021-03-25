c224b11170f1e50b32b9ab78de1b37120c7e7f79
Roman Statsura <srs1012@yandex.ru>
2021-03-25 20:00:53 +0400

    modified task3_1_2

--- Task 3/3_1/Task3_1_2/Task3_1_2/Program.cs
+++ Task 3/3_1/Task3_1_2/Task3_1_2/Program.cs
@@ -8,19 +8,19 @@ namespace Task3_1_2
     {
         static void Main(string[] args)
         {
-            string s = "After a recent move To my home, I realized the scale of the 'tragedy' with unread books." +
-                " Nothing else fits into my cart with unread, which means that I have to tie up (temporarily) " +
-                "shopping and read what I have.";
+            Console.WriteLine("Введите строку: ");
+            string s = Console.ReadLine();
             Dictionary<string, int> QuantityOfRepeatitions = new Dictionary<string, int>();
             string[] mass = Regex.Replace(s, "[-.?!)(,:']", "").ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            int normal = (int)Math.Round(mass.Length * 0.1);
-            Console.WriteLine(normal);
+            int normal = (int)Math.Round(mass.Length * 0.1) + 1;
             foreach (var item in mass)
             {
                 if (QuantityOfRepeatitions.ContainsKey(item)) QuantityOfRepeatitions[item]++;
                 else QuantityOfRepeatitions.Add(item, 1);
             }
-            if (CheckMonotonousText(QuantityOfRepeatitions, normal)) Console.WriteLine("К сожалению, Ваша речь однообразна(");
+            if (CheckMonotonousText(QuantityOfRepeatitions, normal)) 
+                Console.WriteLine($"К сожалению, Ваша речь однообразна. Чаще всего вы используете слово: " +
+                    $"{FindFavoriteWord(QuantityOfRepeatitions)}");
             else Console.WriteLine("Поздравляем! Ваша речь разнообразна!");
             foreach (var item in QuantityOfRepeatitions) Console.WriteLine($"{item.Key} : {item.Value}");
         }
@@ -29,6 +29,13 @@ namespace Task3_1_2
             foreach (var item in d) 
                 if (item.Value >= norm) return true;
             return false;
+        }
+        public static string FindFavoriteWord(Dictionary<string, int> d)
+        {
+            int maxV = 1;
+            foreach (var item in d)
+                if (item.Value > maxV) return item.Key;
+            return "";
         }
     }
 }
